d4de1b7012d0b4984377db457bdd8f66f34c70ab
jakajacky <zhangxiaoqiangjoe@gmail.com>
2018-01-15 09:38:06 +0800

    Initial Commit

---
+++ DRMVVMDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DRMVVMDemo
+//
+//  Created by XiaoQiang on 2018/1/15.
+//  Copyright © 2018年 XiaoQiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DRMVVMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DRMVVMDemo
+//
+//  Created by XiaoQiang on 2018/1/15.
+//  Copyright © 2018年 XiaoQiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
